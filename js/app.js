6ba906c29aa53cdb5401034e1381e4393aafbb3f
Luan-Thanh <akuma.zero000@gmail.comg>
2022-11-09 10:00:36 +0700

    fix link page and hover

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
+const navbar = document.querySelector(".navbar");
 const iconMobile = document.querySelector(".navbar__icon-mobile");
 const listMobile = document.querySelector(".navbar__list-mobile");
 const overlayMobile = document.querySelector(".navbar__overlay-mobile");
 
 function displayMenuOnMobile() {
+  navbar.setAttribute("style", "border-bottom: 1px solid #ffffff;");
   iconMobile.classList.toggle("active");
-  listMobile.classList.toggle("active");
-  overlayMobile.classList.toggle("active");
+  listMobile.classList.toggle("show");
+  overlayMobile.classList.toggle("show");
 }
 
 iconMobile.onclick = displayMenuOnMobile;
 
 overlayMobile.onclick = displayMenuOnMobile;
 
-console.log(iconMobile);
+console.log(navbar.style.borderBottom);
